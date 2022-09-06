650ed6e6fafd57c88e98a95c2167a24df09fe1e2
Kang MinSeok <alstjr8125@naver.com>
2022-09-06 12:24:42 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    // const response = await fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    // );
+    // const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>{movie.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
